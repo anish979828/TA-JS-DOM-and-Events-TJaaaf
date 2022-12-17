a7476967e7e8876c9ba1bf2495296aa9d24b5d51
ANISH KUMAR GIRI <girianish2004@gmail.com>
2022-12-17 22:03:04 +0530

    add layout of game

---
+++ block-BJaaen/code/script.js
@@ -0,0 +1,38 @@
+let user = document.querySelectorAll(".user-icon")
+let computer = document.querySelector(".computer-icon")
+console.log(user)
+
+
+
+// <!-- Rock -->
+// <i class="fa fa-hand-rock-o" aria-hidden="true"></i>
+
+// <!-- Paper -->
+// <i class="fa fa-hand-paper-o" aria-hidden="true"></i>
+
+// <!-- Scissors -->
+// <i class="fa fa-hand-scissors-o" aria-hidden="true"></i>
+
+
+// Scissors cuts Paper
+// Paper covers Rock
+// // Rock crushes Scissors
+
+let dataSet = [
+    {
+     name:"rock",
+     beat:"scissors"
+    },
+    {
+     name:"scissors",
+     beat:"paper"   
+    },
+    {
+     name:"paper",
+     beat:"rock"
+    }
+];
+    // dataSet.forEach(
+
+    // )
+    
