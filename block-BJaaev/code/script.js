f600c0a3822a89170ed763a0836c5589d36341a4
ANISH KUMAR GIRI <girianish2004@gmail.com>
2023-03-11 22:05:41 +0530

    refactor

--- block-BJaaev/code/script.js
+++ block-BJaaev/code/script.js
@@ -15,7 +15,7 @@ function handleInput(event){
     localStorage.setItem('allTodo',JSON.stringify(allTodo));
    }
 }
-// handleChange function \
+// handleChange function 
 function handleChange(event){
     let id = event.target.id;
     allTodo[id].isDone = !allTodo[id].isDone;
@@ -25,9 +25,14 @@ function handleChange(event){
 }
 
 function deleteTodo(event){
-    if(!event.target.classList.contains("delete")) return;
-    var id = event.target.id;
+    // if(!event.target.classList.contains("delete")) return;
+    let  id = event.target.id;
+    console.log(id);
     allTodo.splice(id, 1);
+    // console.log(allTodo);
+    // let a = allTodo.filter(el => id !== el.id);
+    // allTodo = a;
+    console.log(allTodo);
     createUI(allTodo);
     localStorage.setItem('allTodo',JSON.stringify(allTodo))
     footerUI();
@@ -39,11 +44,13 @@ function createUI(data){
     ul.innerHTML = "";
    data.forEach((ele,i) => {
         let li = document.createElement("li");
+        ele.id = i;
         let input = document.createElement("input");
         input.checked = ele.isDone;
         input.type = "checkbox";
         input.classList.add("input");
         input.id = i;
+        li.id = i;
         input.addEventListener("change",handleChange);
         let label = document.createElement("label");
         label.innerText = ele.todo;
@@ -59,6 +66,7 @@ function createUI(data){
     })
 }
 createUI(allTodo);
+// footerUI()
 // footerUI
 let footer = document.querySelector("footer")
 
@@ -95,7 +103,6 @@ function handleClearCompleted(){
 function footerUI(){
     footer.innerHTML = ""
     if(allTodo.length > 0) {
-        console.log(allTodo, 'hekckk')
         let todoCount = document.createElement("span")
         todoCount.innerText = `${filterItem()} items left`
         let filter = document.createElement("ul")
@@ -123,3 +130,92 @@ function footerUI(){
 // Event on display 
 
 display.addEventListener("keyup" , handleInput)
+
+let input = document.querySelector("#text");
+let root = document.querySelector("ul");
+let All = document.querySelector(".All");
+let Completed = document.querySelector(".Completed");
+let Active = document.querySelector(".Active");
+let Clear = document.querySelector(".Clear");
+let item = document.querySelector(".item");
+
+let allTodos = JSON.parse(localStorage.getItem("todos")) || [];
+
+function handleEvent(event) {
+  let value = event.target.value;
+  if (event.keyCode === 13 && event.target.value !== "") {
+    allTodos.push({
+      todo: value,
+      isDone: false,
+    });
+    createUI(allTodos, root);
+    event.target.value = "";
+  }
+  localStorage.setItem("todos", JSON.stringify(allTodos));
+}
+
+// function deleteTodo(event) {
+//   let id = event.target.dataset.id;
+
+//   allTodos.splice(id, 1);
+//   localStorage.setItem("todos", JSON.stringify(allTodos));
+//   createUI(allTodos, root);
+// }
+
+// function handleToggle(event) {
+//   let id = event.target.dataset.id;
+//   allTodos[id].isDone = !allTodos[id].isDone;
+
+//   localStorage.setItem("todos", JSON.stringify(allTodos));
+
+//   createUI(allTodos, root);
+// }
+
+// function handleCompleted(event) {
+//   let completed = allTodos.filter((e) => e.isDone);
+//   createUI(completed);
+// }
+// function handleActive() {
+//   let active = allTodos.filter((e) => !e.isDone);
+//   createUI(active);
+// }
+// function handleALl() {
+//   createUI();
+// }
+
+// function createUI(data = allTodos, rootElm = root) {
+//   rootElm.innerHTML = "";
+//   data.forEach((elm, index) => {
+//     let li = document.createElement("li");
+//     let inp = document.createElement("input");
+//     inp.type = "checkbox";
+//     inp.checked = elm.isDone;
+//     inp.setAttribute("data-id", index);
+//     inp.addEventListener("input", handleToggle);
+
+//     let p = document.createElement("p");
+//     p.innerText = elm.todo;
+//     if (elm.isDone == true) {
+//       p.classList.add("checked");
+//     }
+//     let div = document.createElement("div");
+//     let span = document.createElement("span");
+//     span.innerText = "X";
+//     span.innerText = "X";
+//     span.setAttribute("data-id", index);
+
+//     span.addEventListener("click", deleteTodo);
+
+//     div.append(span);
+
+//     li.append(inp, p, div);
+//     root.append(li);
+
+//     item.innerText = `${allTodos.filter((e) => !e.isDone).length} items left`;
+//   });
+// }
+
+// input.addEventListener("keyup", handleEvent);
+// Completed.addEventListener("click", handleCompleted);
+// Active.addEventListener("click", handleActive);
+// All.addEventListener("click", handleALl);
